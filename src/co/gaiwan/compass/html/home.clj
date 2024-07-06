66ce3086ebbf06c909938a7f93370ed89377dca6
Laurence Chen <humorless@gmail.com>
2024-07-06 19:06:43 +0800

    connect the session data from database to frontend

--- src/co/gaiwan/compass/html/home.clj
+++ src/co/gaiwan/compass/html/home.clj
@@ -6,17 +6,57 @@
    [co.gaiwan.compass.html.sessions :as sessions]
    [co.gaiwan.compass.css.tokens :as t :refer :all]
    [co.gaiwan.compass.http.oauth :as oauth]
+   [co.gaiwan.compass.db :as db]
+   [co.gaiwan.compass.util :as util]
    [lambdaisland.ornament :as o]))
 
+(def session-query
+  '[:find (pull ?e [*]) ?type-keyword ?location-name
+    :where [?e :session/name]
+    [?e :session/location ?l]
+    [?l :location/name ?location-name]
+    [?e :session/type ?t]
+    [?t :db/ident ?type-keyword]])
+
+(defn session-model
+  "session data from database to frontend
+  
+   input `x` is a vecotr of 
+         `[session-graph type-keyword location-name]`"
+  [v]
+  (let [[session-graph type-keyword location-name] v
+        {:session/keys [time speaker name capacity organized
+                        duration]} session-graph
+        {:keys [day-of-week month day-of-month
+                time]} (util/format-instant time)
+        time-str (format "%02d.%02d" (.getHour time) (.getMinute time))
+        date-str (format "%02d.%02d" month day-of-month)]
+    {:title name
+     :speaker speaker
+     :type type-keyword
+     :organized organized
+     :day (.name day-of-week)
+     :date date-str
+     :time time-str
+     :location location-name}))
 
 (o/defstyled home :div
   [sessions/session-card :mb-3]
   ([user]
    [:<>
     [:main
-     [sessions/session-card (sessions/rand-session)]
-     [sessions/session-card (sessions/rand-session)]
-     [sessions/session-card (sessions/rand-session)]
-     [sessions/session-card (sessions/rand-session)]
-     [sessions/session-card (sessions/rand-session)]]
-    ]))
+     ;; replace 
+     ;; `[sessions/session-card (sessions/rand-session)]`
+     ;; with the following let block
+     (let [session-data (db/q session-query (db/db))]
+       (map
+        (fn [x]
+          [sessions/session-card
+           (session-model x)])
+        session-data))]]))
+
+(comment
+  [sessions/session-card (sessions/rand-session)]
+  [sessions/session-card
+   (session-model
+    (first (db/q session-query (db/db))))])
--- src/co/gaiwan/compass/util.clj
+++ src/co/gaiwan/compass/util.clj
@@ -1,6 +1,33 @@
 (ns co.gaiwan.compass.util
+  (:import [java.time Instant ZonedDateTime ZoneId]
+           [java.time.format DateTimeFormatter])
   (:require
    [clojure.pprint :as pprint]))
+
+(defn format-instant
+  "Output: 
+  
+  ```
+  {:time #object[java.time.LocalTime],
+   :day-of-week #object[java.time.DayOfWeek],
+   :month LONG,
+   :day-of-month LONG}
+  ```"
+  [instant]
+  (let [zone-id (ZoneId/systemDefault)
+        zdt (ZonedDateTime/ofInstant instant zone-id)
+        day-of-week (.getDayOfWeek zdt)
+        month (.getMonthValue zdt)
+        day-of-month (.getDayOfMonth zdt)
+        time (.toLocalTime zdt)]
+     {:time time
+      :day-of-week day-of-week
+      :month month
+      :day-of-month day-of-month}))
+
+(comment
+  (format-instant (Instant/now))
+  )
 
 (defn pprint-str [o]
   (with-out-str (pprint/pprint o)))
