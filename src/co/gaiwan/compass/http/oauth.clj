e5ed3a76571168e73a6d8642d085a3e3610e9f87
JohnnyJayJay <johnnyjayjay02@gmail.com>
2024-07-20 16:29:11 +0200

    add function to get current access token and refresh if necessary

--- src/co/gaiwan/compass/http/oauth.clj
+++ src/co/gaiwan/compass/http/oauth.clj
@@ -7,14 +7,15 @@
    [datomic.api :as d]
    [hato.client :as hato]
    [lambdaisland.uri :as uri]
-   [ring.util.response :as response]))
+   [ring.util.response :as response])
+  (:import (java.time Instant)))
 
 (def discord-oauth-endpoint "https://discord.com/oauth2/authorize")
 (def discord-api-endpoint "https://discord.com/api/v10")
 
 (defn flow-init-url
   ([]
-   (flow-init-url ["email" "identify"]))
+   (flow-init-url ["email" "identify" "guilds.join" "role_connections.write"]))
   ([scopes]
    (-> (uri/uri discord-oauth-endpoint)
        (uri/assoc-query*
@@ -27,17 +28,57 @@
 #_
 (println (str (flow-init-url ["bot" "applications.commands"])))
 
-(defn exchange-code [code]
+(defn request-token [params]
   (hato/post
    (str discord-api-endpoint "/oauth2/token")
    {:as :auto
-    :form-params
-    {:grant_type "authorization_code"
-     :code code
-     :redirect_uri (str (config/value :compass/origin) "/oauth2/discord/callback")}
+    :form-params params
     :basic-auth
     {:user (config/value :discord/client-id)
      :pass (config/value :discord/client-secret)}}))
+
+(defn exchange-code [code]
+  (request-token
+   {:grant_type "authorization_code"
+    :code code
+    :redirect_uri (str (config/value :compass/origin) "/oauth2/discord/callback")}))
+
+(defn refresh-token [refresh-token]
+  (request-token
+   {:grant_type "refresh_token"
+    :refresh_token refresh-token}))
+
+(defn- expires-in->instant
+  [expires-in]
+  (.plusSeconds (Instant/now) (- expires-in 60)))
+
+(defn current-access-token
+  "Get a valid Discord bearer access token for a user.
+
+  This assumes there already is an access token in the database.
+  The function performs a token refresh if the old access token is considered to be expired.
+  If refreshing the token fails, returns `nil`.
+  There are no guarantees regarding the lifetime of the returned access token, so this function
+  should be called for every request to Discord."
+  [user-id]
+  (let [oauth-data
+        (db/q '[:find (pull ?u [:discord/access-token
+                                :discord/refresh-token
+                                :discord/expires-at])
+                :in $ ?uid
+                :where [?u :user/uuid ?uid]]
+              (db/db)
+              user-id)]
+    ;; check if token is expired, do a refresh in that case
+    (if (.isAfter (Instant/now) (:discord/expires-at oauth-data))
+      (let [{:keys [body status]} (refresh-token (:discord/refresh-token oauth-data))]
+        (when (= status 200)
+          (db/transact [{:user/uuid user-id
+                         :discord/access-token (:access_token body)
+                         :discord/refresh-token (:refresh_token body)
+                         :discord/expires-at (expires-in->instant (:discord/expires_in body))}])
+          (:access_token body)))
+      (:discord/access-token oauth-data))))
 
 (defn fetch-user-info [token]
   (:body
@@ -66,7 +107,7 @@
               :discord/id            id
               :discord/access-token  access_token
               :discord/refresh-token refresh_token
-              :discord/expires-at    (.plusSeconds (java.time.Instant/now) (- expires_in 60))}]]
+              :discord/expires-at (expires-in->instant expires_in)}]]
         (def tx-data tx-data)
         @(db/transact tx-data )
         {:status  302
