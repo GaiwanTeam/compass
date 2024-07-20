8cc3c3a01f2949b7443e0df04ee8cfa5e105da8a
JohnnyJayJay <johnnyjayjay02@gmail.com>
2024-07-20 16:29:11 +0200

    add function to get current access token and refresh if necessary

--- src/co/gaiwan/compass/http/oauth.clj
+++ src/co/gaiwan/compass/http/oauth.clj
@@ -3,12 +3,17 @@
    [clojure.string :as str]
    [co.gaiwan.compass.config :as config]
    [co.gaiwan.compass.db :as db]
-   [lambdaisland.uri :as uri]))
+   [co.gaiwan.compass.util :as util]
+   [datomic.api :as d]
+   [hato.client :as hato]
+   [lambdaisland.uri :as uri]
+   [ring.util.response :as response])
+  (:import (java.time Instant)))
 
 (def discord-oauth-endpoint "https://discord.com/oauth2/authorize")
 (def discord-api-endpoint "https://discord.com/api/v10")
 
-(def default-scopes  ["email" "identify"])
+(def default-scopes  ["email" "identify" "guilds.join" "role_connections.write"])
 
 (defn flow-init-url
   ([]
@@ -27,3 +32,106 @@
            :scope         (str/join " " scopes)
            :state         state})))))
 
+;; Add as bot to server
+#_
+(println (str (flow-init-url ["bot" "applications.commands"])))
+
+(defn request-token [params]
+  (hato/post
+   (str discord-api-endpoint "/oauth2/token")
+   {:as :auto
+    :form-params params
+    :basic-auth
+    {:user (config/value :discord/client-id)
+     :pass (config/value :discord/client-secret)}}))
+
+(defn exchange-code [code]
+  (request-token
+   {:grant_type "authorization_code"
+    :code code
+    :redirect_uri (str (config/value :compass/origin) "/oauth2/discord/callback")}))
+
+(defn refresh-token [refresh-token]
+  (request-token
+   {:grant_type "refresh_token"
+    :refresh_token refresh-token}))
+
+(defn- expires-in->instant
+  [expires-in]
+  (.plusSeconds (Instant/now) (- expires-in 60)))
+
+(defn current-access-token
+  "Get a valid Discord bearer access token for a user.
+
+  This assumes there already is an access token in the database.
+  The function performs a token refresh if the old access token is considered to be expired.
+  If refreshing the token fails, returns `nil`.
+  There are no guarantees regarding the lifetime of the returned access token, so this function
+  should be called for every request to Discord."
+  [user-id]
+  (let [oauth-data
+        (db/q '[:find (pull ?u [:discord/access-token
+                                :discord/refresh-token
+                                :discord/expires-at])
+                :in $ ?uid
+                :where [?u :user/uuid ?uid]]
+              (db/db)
+              user-id)]
+    ;; check if token is expired, do a refresh in that case
+    (if (.isAfter (Instant/now) (:discord/expires-at oauth-data))
+      (let [{:keys [body status]} (refresh-token (:discord/refresh-token oauth-data))]
+        (when (= status 200)
+          (db/transact [{:user/uuid user-id
+                         :discord/access-token (:access_token body)
+                         :discord/refresh-token (:refresh_token body)
+                         :discord/expires-at (expires-in->instant (:discord/expires_in body))}])
+          (:access_token body)))
+      (:discord/access-token oauth-data))))
+
+(defn fetch-user-info [token]
+  (:body
+   (hato/get (str discord-api-endpoint "/users/@me")
+             {:as :auto
+              :headers {"Authorization" (str "Bearer " token)}})))
+
+(defn GET-callback [{:keys [query-params]}]
+  (let [code                  (get query-params "code")
+        {:keys [status body]} (exchange-code code)]
+    (if (not= 200 status)
+      {:status  302
+       :headers {"Location" "/"}
+       :flash   [:p
+                 "Discord OAuth2 exchange failed."
+                 [:pre (util/pprint-str body)]]
+       :session {:identity nil}}
+      (let [{:keys [access_token refresh_token expires_in]} body
+            {:keys [id global_name email username]}         (fetch-user-info access_token)
+            user-uuid                                       (:user/uuid (d/entity (db/db) [:user/email email]) (random-uuid))
+            tx-data
+            [{:user/uuid             user-uuid
+              :user/email            email
+              :user/name             global_name
+              :user/handle           username
+              :discord/id            id
+              :discord/access-token  access_token
+              :discord/refresh-token refresh_token
+              :discord/expires-at (expires-in->instant expires_in)}]]
+        (def tx-data tx-data)
+        @(db/transact tx-data )
+        {:status  302
+         :headers {"Location" "/"}
+         :flash   [:p "You are signed in!"]
+         :session {:identity user-uuid}}))))
+
+(defn routes []
+  [""
+   ["/oauth2"
+    ["/discord"
+     ["/callback"
+      {:get {:handler GET-callback}}]]]
+   ["/logout"
+    {:get {:handler (fn [req]
+                      (assoc
+                       (response/redirect "/")
+                       :flash "You were signed out"
+                       :session {}))}}]])
