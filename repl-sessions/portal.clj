75b647de740b6d9b8fb1e5fa784067eddf4c4176
Laurence Chen <humorless@gmail.com>
2024-08-08 22:55:37 +0800

    Revert "add portal utility"
    This reverts commit ea69b3be91e61c929ab2d4f8e083f42c47a8f88a.

--- repl-sessions/portal.clj
+++ repl-sessions/portal.clj
@@ -1,26 +1,9 @@
 (ns portal)
 
+
 (clojure.java.shell/sh
  "/home/arne/opt/firefox-nightly/firefox"
  (portal.runtime.jvm.launcher/url @user/portal-instance)
  :env {"DISPLAY" ":0"})
 
 (println (portal.runtime.jvm.launcher/url @user/portal-instance))
-
-;;
-(require '[portal.api :as p])
-
-(def portal-instance (atom nil))
-
-(defn portal
-  "Open a Portal window and register a tap handler for it. The result can be
-  treated like an atom."
-  []
-  ;; Portal doesn't recognize records as maps, make them at least datafiable
-  (extend-protocol clojure.core.protocols/Datafiable
-    clojure.lang.IRecord
-    (datafy [r] (into {} r)))
-  (let [pi (p/open @portal-instance)]
-    (reset! portal-instance pi)
-    (add-tap p/submit)
-    pi))
