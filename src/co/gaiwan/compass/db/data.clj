166c3f27d1bc25f8c0e9f83adc44dc64df341a82
Arne Brasseur <arne@arnebrasseur.net>
2024-08-29 11:05:11 +0200

    Bring back old location stub data for now

--- src/co/gaiwan/compass/db/data.clj
+++ src/co/gaiwan/compass/db/data.clj
@@ -8,8 +8,28 @@
 (require 'java-time-literals.core)
 
 (defn locations []
-  [{:location/name "Het Depot"}
-   {:location/name "Hal 5"}])
+  [{:location/name "Het Depot"
+    :db/ident      :location.type/depot}
+   {:location/name "Het Depot - main stage"
+    :db/ident      :location.type/depot-main-stage}
+   {:location/name "Het Depot - Bar"
+    :db/ident      :location.type/depot-bar}
+   {:location/name "Hal 5"
+    :db/ident      :location.type/hal5}
+   {:location/name "Hal 5 - zone A"
+    :db/ident      :location.type/hal5-zone-a}
+   {:location/name "Hal 5 - zone B"
+    :db/ident      :location.type/hal5-zone-b}
+   {:location/name "Hal 5 - HoC Café"
+    :db/ident      :location.type/hal5-hoc-cafe}
+   {:location/name "Hal 5 - Foodcourt"
+    :db/ident      :location.type/hal5-foodcourt}
+   {:location/name "Hal 5 - park"
+    :db/ident      :location.type/hal5-park}
+   {:location/name "Hal 5 - outside seating"
+    :db/ident      :location.type/hal5-outside-seating}
+   {:location/name "Hal 5 - long table"
+    :db/ident      :location.type/hal5-long-table}])
 
 (defn session-types []
   [{:session.type/name  "Talk"
